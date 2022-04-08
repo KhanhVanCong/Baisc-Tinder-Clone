cb183f70dc0a0168772547156f9eca1fbbb63f97
khanhvancong <khanhvc2310@gmail.com>
2022-04-08 16:53:15 +0700

    implement upload avatar for user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -65,6 +65,9 @@ class UsersController < ApplicationController
 
   # Only allow a list of trusted parameters through.
   def user_params
-    params.require(:user).permit(:first_name, :last_name, :email, :age, :password, :password_confirmation)
+    params.require(:user).permit(:first_name, :last_name,
+                                 :email, :age,
+                                 :password, :password_confirmation,
+                                 :avatar)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  has_one_attached :avatar
+
   validates :first_name, presence: true, length: { maximum: 50 }
   validates :last_name, presence: true, length: { maximum: 50 }
   validates :age, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 18,
@@ -9,11 +11,16 @@ class User < ApplicationRecord
             uniqueness: true
   has_secure_password
   validates :password, length: { minimum: 6 }
+  validates :avatar, presence: true
 
   class << self
     def digest(string)
       cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine::cost
       BCrypt::Password.create(string, cost: cost)
     end
+  end
+
+  def avatar_as_thumbnail
+    avatar.variant(resize_to_limit: [300, 300]).processed
   end
 end
